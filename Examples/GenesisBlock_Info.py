d5ee922e2cc50fc21dfebfed0506b6507a4a05d7
Oliver Morris <officialgamermorris@gmail.com>
2017-09-25 17:07:23 +0100

    Update GenesisBlock_Info.py

--- Examples/GenesisBlock_Info.py
+++ Examples/GenesisBlock_Info.py
@@ -2,7 +2,7 @@
 import maxcoinrpc.connection
 
 ## connection to the rpc
-conn = maxcoinrpc.connection.MaxcoinConnection('rpcuser', 'rpcpassword', host='localhost', port=8080, use_https=False)
+conn = maxcoinrpc.connection.MaxcoinConnection('rpcuser', 'rpcpassword', host='localhost', port=rpcport, use_https=False)
 
 ## Returns information about the genesis block.
 genesisblock = conn.getblock('000000bed6f953b6cf20afd8dc1d965dbf007587b119ff794f4b26755ebacc6b')
