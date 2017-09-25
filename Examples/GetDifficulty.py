7041cf55e086c5e134e7515c68c11e1a95b450a4
Oliver Morris <officialgamermorris@gmail.com>
2017-09-25 17:08:37 +0100

    Update GetDifficulty.py

--- Examples/GetDifficulty.py
+++ Examples/GetDifficulty.py
@@ -2,6 +2,7 @@
 import maxcoinrpc.connection
 
 ## connection to the rpc
+## Remember to replace the rpcuser, rpcpassword and port number, to your info
 conn = maxcoinrpc.connection.MaxcoinConnection('rpcuser goes here', 'rpcpassword goes here', host='localhost', port=rpcportnumber, use_https=False)
 
 ## Get difficulty from the rpc connection
