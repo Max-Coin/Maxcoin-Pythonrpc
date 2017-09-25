2fac4d54e3fc860ef472d6e6eefaa0d2fa1d4778
Oliver Morris <officialgamermorris@gmail.com>
2017-09-25 17:09:33 +0100

    Update rpcConnection.py

--- Examples/rpcConnection.py
+++ Examples/rpcConnection.py
@@ -1,8 +1,11 @@
 import maxcoinrpc.connection
 
+## Remember to replace the rpcuser, rpcpassword and port number, to your info
 conn = maxcoinrpc.connection.MaxcoinConnection('rpcuser goes here', 'rpcpassword goes here', host='localhost', port=rpcportnumber, use_https=False)
 
+## Get Mininginfo
 print conn.getmininginfo()
 
+## Getinfo
 print conn.getinfo()
 
